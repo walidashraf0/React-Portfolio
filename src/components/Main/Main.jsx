84072beed6dc651695fecd7de6edb94656fe5064
walid <walidashraf050@gmail.com>
2024-05-17 07:44:25 +0300

    Main Section Important

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -10,10 +10,30 @@ export default function Main() {
         <button className="btn">React & MUI</button>
         <button className="btn">Node % Express</button>
       </div>
-      <div className="right-section borders">
-        <div className="card">
-          <img src="" alt="" />
+      <div className="right-section d-flex">
+        {["aa", "bb", "cc", "dd", "ee", "ff"].map((card, index) => {
+          return (<div key={index} className="card">
+          <img src="/images/1.jpg" alt="" />
+          <div className="info">
+            <h1 className="title">Landing Page 2</h1>
+            <p className="subtitle">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deleniti cum ullam facilis perferendis perspiciatis quas, quia doloremque dicta consequuntur aspernatur.</p>
+            <div className="details d-flex justify-content-between">
+              <div className="icons">
+                <a className="link" href="">
+                    <i className="fa-solid fa-link"></i>
+                  </a>
+                  <a href="https://github.com/walidashraf0">
+                    <i className="github fa-brands fa-github"></i>
+                  </a>
+              </div>
+              <div className="more d-flex">
+                <span>More</span>
+                <i className="fa-solid fa-arrow-right"></i>
+              </div>
+            </div>
+          </div>
         </div>
+        )})}
       </div>
     </main>
   );
